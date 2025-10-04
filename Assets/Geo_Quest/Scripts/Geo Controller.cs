1fb5598eedcde756a116c12fae27d59d527a09a8
WtverDann4 <dannasanchez2435@gmail.com>
2025-10-04 12:27:51 -0400

    work #5
    edit script

--- Assets/Geo_Quest/Scripts/Geo Controller.cs
+++ Assets/Geo_Quest/Scripts/Geo Controller.cs
@@ -8,18 +8,60 @@ public class Geo : MonoBehaviour
 {
     string varOne = "Hello";
 
+    private Rigidbody2D rb;
+
+    public int speed = 5;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+    }
     // Start is called before the first frame update
     void Start( )
-
     {
-        string varTwo = "World";
-        Debug.Log(varOne + varTwo);
+        rb = GetComponent<Rigidbody2D>();
 
     }
-
+    int var = 3;
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        float xInput = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+
+
+       /*
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1, rb.velocity.y);
+        }
+     
+         ------------------------------------
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0,1,0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        */
     }
+
+
+
+
 }
