193627fd080c646c1098893c2bab8b908b8ef295
WtverDann4 <dannasanchez2435@gmail.com>
2025-10-11 12:32:22 -0400

    CW Project # 6
    scene_1,2,and 3

--- Assets/Geo_Quest/Scripts/Geo Controller.cs
+++ Assets/Geo_Quest/Scripts/Geo Controller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Geo : MonoBehaviour
 {
@@ -12,56 +13,80 @@ public class Geo : MonoBehaviour
 
     public int speed = 5;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log("Hit");
-    }
+    public string nextlevel = "Scene_2";
+
+
     // Start is called before the first frame update
-    void Start( )
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
     }
     int var = 3;
     // Update is called once per frame
-    void Update() 
+    void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
 
 
-       /*
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            rb.velocity = new Vector2(-1, rb.velocity.y);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            rb.velocity = new Vector2(1, rb.velocity.y);
-        }
-     
-         ------------------------------------
+        /*
+         if (Input.GetKeyDown(KeyCode.A))
+         {
+             rb.velocity = new Vector2(-1, rb.velocity.y);
+         }
+         if (Input.GetKeyDown(KeyCode.D))
+         {
+             rb.velocity = new Vector2(1, rb.velocity.y);
+         }
 
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position += new Vector3(0,1,0);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-        }
-        if(Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += new Vector3(1, 0, 0);
-        }
-        */
+          ------------------------------------
+
+         if (Input.GetKeyDown(KeyCode.W))
+         {
+             transform.position += new Vector3(0,1,0);
+         }
+         if (Input.GetKeyDown(KeyCode.S))
+         {
+             transform.position += new Vector3(0, -1, 0);
+         }
+         if (Input.GetKeyDown(KeyCode.A))
+         {
+             transform.position += new Vector3(-1, 0, 0);
+         }
+         if(Input.GetKeyDown(KeyCode.D))
+         {
+             transform.position += new Vector3(1, 0, 0);
+         }
+         */
+
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thislevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thislevel);
+                   
+                    Debug.Log("Death");
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextlevel);
+                    break;
 
 
+                }
 
+               
+                  
+
+        }
+
+
+    }
 }
