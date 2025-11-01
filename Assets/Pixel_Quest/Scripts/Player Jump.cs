3715ebb268e22032daad4e44516ec6f1b5a4edb8
WtverDann4 <dannasanchez2435@gmail.com>
2025-11-01 12:19:35 -0400

    CW # 8

--- Assets/Pixel_Quest/Scripts/Player Jump.cs
+++ Assets/Pixel_Quest/Scripts/Player Jump.cs
@@ -1,18 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Playerjump : MonoBehaviour
-{
+{     //Components
+    private Rigidbody2D rb;
+    //forces
+    public float jumpForce = 10;
+    public float fallForce = 2;
+    private Vector2 _gravityVector;
+    //Capsule
+    public float CapsuleHeight = 0.25f;
+    public float CapsuleRadius = 0.08f;
+    //Ground Check
+    public Transform feetCollider;
+    public LayerMask groundMask;
+    private bool _groundCheck;
+    //Water check
+    public bool _waterCheck;
+    private string _waterTag = "Water";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _gravityVector = new Vector2(0, Physics2D.gravity.y);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
+        //Checks if player is touching ground
+        _groundCheck = Physics2D.OverlapCapsule(feetCollider.position,
+        new Vector2(CapsuleHeight, CapsuleRadius), CapsuleDirection2D.Horizontal,
+       0, groundMask);
+        // Checks if the player is trying to jump/can jump
+        if (Input.GetKeyDown(KeyCode.Space) && (_groundCheck || _waterCheck))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+        }
+        //chcks if the gravity should be getting faster
+        if (rb.velocity.y < 0 && !_waterCheck)
+        {
+            rb.velocity += _gravityVector * (fallForce * Time.deltaTime);
+
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(_waterTag)) { _waterCheck = true; }
+
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(_waterTag)) { _waterCheck = false; }
+
+
     }
 }
--- Assets/Pixel_Quest/Scripts/PlayerMovement.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMovement.cs
@@ -5,16 +5,22 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     public int speed = 5;
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer; 
     // Start is called before the first frame update
-    void Start()
+     void Start()
+
     {
         rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        if (xInput > 0) { _spriteRenderer.flipX = true; }
+        else if (xInput < 0) { _spriteRenderer.flipX = false; }
+            rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
     }
 }
